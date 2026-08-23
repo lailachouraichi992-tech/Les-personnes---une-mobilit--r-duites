3d2f5f0330a0a813d01c3b9e98e2e5d3a92dd2d8
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-08-23 21:58:16 +0100

    Update headings and images in travail.tsx

--- src/components/travail.tsx
+++ src/components/travail.tsx
@@ -11,58 +11,57 @@ export default function Travail(){
         <>  
          
         
-     <h1 className="titre-1">Nos Services </h1>
+     <h1 id="titre-1">Nos-droits-au-Travail</h1>
         
-    <div className="continer-1">
+    <div id="continer-1">
 
-        <p className="Second-paragraph"> Loi_d'education</p>
-        <p id="Last-paragraph">Loi-cadre 97-13 <br />
+        <p id="Second-paragraph"> Loi_de-travail</p>
+        <p className="Last-paragraph">Loi-cadre 97-13 <br />
        Elle vise à protéger les droits des personnes en situation de handicap, y compris le droit à l’emploi et à l’insertion professionnelle.</p>
 
 
-        <img  className="img" src="/img/image des services/disability-kids-school-educational-projects-260nw-2036936300.webp" alt=""/>
+        <img  className="img" src="/img/images-des-services/istockphoto-1483052139-612x612.jpg" alt=""/>
     
     </div>
 
 
-    <div  className="continer-2">
+    <div id="continer-2">
 
-        <p className="Second-paragraph"> Principe d’égalité </p>
-        <p id="Last-paragraph">Les personnes en situation de handicap ont le droit : <br />
+        <p id="Second-paragraph"> Principe d’égalité </p>
+        <p className="Last-paragraph">Les personnes en situation de handicap ont le droit : <br />
         D’accéder à un emploi sans discrimination <br />
         De bénéficier d’égalité des chances <br />
         D’obtenir des aménagements raisonnables sur le lieu de travail</p>
 
 
-        <img  className="img" src="/img/image des services/disability-kids-school-educational-projects-260nw-2036936300.webp" alt=""/>
+        <img  className="img" src="/img/images-des-services/istockphoto-1483052139-612x612.jpg" alt=""/>
     
     </div>
 
 
-    <div className="continer-3">
+    <div id="continer-3">
 
-              <p className="Second-paragraph"> Mesures et politiques publiques</p>
-        <p id="Last-paragraph"> Parmi les mesures prévues : <br />
+        <p id="Second-paragraph"> Mesures et politiques publiques</p>
+        <p className="Last-paragraph"> Parmi les mesures prévues : <br />
        Promotion de l’insertion professionnelle <br />
        Encouragement de l’auto-emploi <br />
-      Programmes de formation professionnelle adaptés</p>
+       Programmes de formation professionnelle adaptés</p>
 
-
-        <img  className="img" src="/img/image des services/disability-kids-school-educational-projects-260nw-2036936300.webp" alt=""/>
+        <img className="img" src="/img/images-des-services/istockphoto-1483052139-612x612.jpg" alt=""/>
     
     </div>
 
 
-    <div  className="continer-4">
+    <div  id="continer-4">
 
-          <p className="Second-paragraph">Importance du droit à l’emploi</p>
-        <p id="Last-paragraph"> Le travail permet : <br />
+          <p id="Second-paragraph">Importance du droit à l’emploi</p>
+        <p className="Last-paragraph"> Le travail permet : <br />
        L’autonomie financière <br />
         L’intégration sociale <br />
        La participation active au développement économique</p>
 
 
-        <img  className="img" src="/img/image des services/disability-kids-school-educational-projects-260nw-2036936300.webp" alt=""/>
+        <img  className="img" src="/img/images-des-services/istockphoto-1483052139-612x612.jpg" alt=""/>
     
     </div>
        
